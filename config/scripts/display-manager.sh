73a1d0545358edca8433a54b76e1f779f89b68be
Nick Saika <n@nesv.ca>
2024-01-18 21:45:54 -0500

    scripts/display-manager: Simplify

--- config/scripts/display-manager.sh
+++ config/scripts/display-manager.sh
@@ -2,16 +2,5 @@
 
 set -euo pipefail
 
-GDM_ENABLED=$(systemctl is-enabled gdm.service)
-if [[ "${GDM_ENABLED}" == "enabled" ]]
-then
-    echo "Disabling gdm.service"
-    systemctl disable gdm.service
-fi
-
-SDDM_ENABLED=$(systemctl is-enabled sddm.service)
-if [[ "${SDDM_ENABLED}" != "enabled" ]]
-then
-    echo "Enabling sddm.service"
-    systemctl enable sddm.service
-fi
+systemctl disable gdm.service
+systemctl enable sddm.service
